4d4a7996c2e5dcb058cd6af487b2ecd3fddf1151
David Orr <davidyorr@gmail.com>
2019-03-16 06:49:55 +0000

    Add bootstrap-ubuntu.sh file

---
+++ bootstrap-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update
+apt-get install --yes software-properties-common
+apt-add-repository --yes --update ppa:ansible/ansible
+apt-get install --yes ansible
+
+ansible-playbook ansible/bootstrap.yml -K
